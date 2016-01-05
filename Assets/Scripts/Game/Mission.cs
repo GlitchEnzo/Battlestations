e5721a62e74865c69f02f28e0691b071b61fc871
GlitchEnzo <mccarthp@gmail.com>
2016-01-04 19:22:27 -0800

    Added states to allow a full mission cycle

--- Assets/Scripts/Game/Mission.cs
+++ Assets/Scripts/Game/Mission.cs
@@ -1,6 +1,7 @@
 ﻿namespace Battlestations
 {
     using System.Collections.Generic;
+    using Gamelogic.Grids;
     using UnityEngine;
 
     public class Mission : MonoBehaviour
@@ -22,10 +23,40 @@
         public List<Character> NonPlayerCharacters;
 
         // TODO: Space map
+        protected WrappedGrid<GameObject, FlatHexPoint> grid;
+        protected IMap3D<FlatHexPoint> map;
 
         public virtual void Assemble()
         {
+            // allow selection of player characters and ship
 
+            // build the space map
+            GameObject cellPrefab = Resources.Load<GameObject>("SpaceCell");
+
+            grid = FlatHexGrid<GameObject>.WrappedHexagon(15);
+
+            map = new FlatHexMap(cellPrefab.GetComponent<SpriteCell>().Dimensions)
+                .WithWindow(new Rect(0, 0, 0, 0))
+                .AlignMiddleCenter(grid)
+                .To3DXY();
+
+            grid.Fill(MakeCell);
+
+            // add and configure camera?
+        }
+
+        private GameObject MakeCell(FlatHexPoint point)
+        {
+            GameObject cellPrefab = Resources.Load<GameObject>("SpaceCell");
+
+            var cell = Instantiate(cellPrefab);
+
+            cell.transform.parent = transform;
+            cell.transform.localPosition = map[point];
+            //ResetCellColor(cell);
+            cell.name = point.ToString();
+
+            return cell;
         }
 
         public virtual EndMissionState CheckEndOfMission()
@@ -44,16 +75,21 @@
             {
                 case MissionState.Assemble:
                     // TODO: Allow the selection of player characters, and player ship
+                    Assemble();
+                    CurrentMissionState++;
                     break;
                 case MissionState.DetermineUpgrades:
                     // TODO: Place players in appropriate modules for upgrades
                     // NOTE: One player or bot MUST be at the hyperdrive battlestation
+                    CurrentMissionState++;
                     break;
                 case MissionState.WarpIn:
                     // TODO: Determine the warp in location and orientation based on dice rolls
+                    CurrentMissionState++;
                     break;
                 case MissionState.ResolveUpgrades:
                     // TODO: Resolve any upgrades based on dice rolls
+                    CurrentMissionState++;
                     break;
                 case MissionState.MissionLoop:
                     UpdateRound();
@@ -66,6 +102,32 @@
                     ResolveEndOfMission();
                     CurrentMissionState++;
                     break;
+                case MissionState.SpoilsOfWar:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.ReviveDeadCharacters:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.RewardExperience:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.RepairAndHeal:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.Requisitioning:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.ReturnCapturedCrew:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.PurchaseEquipment:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.RestoreLuckAndAbilityPools:
+                    CurrentMissionState++;
+                    break;
+                case MissionState.MissionComplete:
+                    break;
             }
         }
 
@@ -75,7 +137,8 @@
             {
                 case RoundStep.PowerGeneration:
                     // TODO: Show animation for player ship generating power?
-                    PlayerShip.GeneratePower();
+                    if (PlayerShip != null)
+                        PlayerShip.GeneratePower();
 
                     foreach (var ship in NPCShips)
                     {
@@ -94,7 +157,8 @@
                     UpdatePhase();
                     break;
                 case RoundStep.Bookkeeping:
-                    PlayerShip.EndRound();
+                    if (PlayerShip != null)
+                        PlayerShip.EndRound();
 
                     foreach (var ship in NPCShips)
                     {
--- Assets/Scripts/Missions/Showdown.cs
+++ Assets/Scripts/Missions/Showdown.cs
@@ -8,15 +8,33 @@
         public override void Assemble()
         {
             base.Assemble();
+
+            // add planet at center
+
+            // add ship orbiting it
         }
 
         public override EndMissionState CheckEndOfMission()
         {
-            return base.CheckEndOfMission();
+            // Success = enemy ship destroyed, retreated, or captured
+            // Fail = players all killed or player ship destroyed
+
+            EndMissionState end = EndMissionState.MissionInProgress;
+
+            if (CurrentRound >= 100)
+            {
+                end = EndMissionState.MissionFail;
+            }
+
+            return end;
         }
 
         public override void ResolveEndOfMission()
         {
+            // Success = enemy ship destroyed or retreated
+            // Overwhelming = enemy ship captured
+            // Fail = players all killed or player ship destroyed
+
             base.ResolveEndOfMission();
         }
     }
